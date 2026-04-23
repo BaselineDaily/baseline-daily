fbca3cc21fe9d9ebd9d5088df229d22f0fd685e2
Jesus Ramos <ramos.jesusroman@gmail.com>
2026-04-23 01:25:45 -0600

    fix: add favicon

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useAuth } from '../lib/AuthContext.tsx'
+import { useAuth } from '../lib/AuthContext'
 
 export default function Login() {
   const { signIn } = useAuth()
